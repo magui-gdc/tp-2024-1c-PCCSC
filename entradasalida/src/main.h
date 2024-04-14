8ffc69d215a0d7c6621f5705a6925701faedd685
mcabrera22 <martincho.cabrera22@gmail.com>
2024-04-14 18:42:20 -0300

    ADD: cargar_config_struct_IO(t_config*)

--- entradasalida/src/main.h
+++ entradasalida/src/main.h
@@ -1,1 +1,19 @@
 #include <utils/utilsCliente.h>
+#include <commons/config.h>
+
+typedef struct
+{
+    char* tipo_interfaz,
+    char* tiempo_unidad_trabajo,
+    char* ip_kernel, 
+    char* puerto_kernel, 
+    char* ip_memoria,
+    char* puerto_memoria,
+    char* path_base_dialfs,
+    char* block_size
+    char* block_count,
+} config_struct_IO;
+
+extern config_struct_IO config;
+
+void cargar_config_struct_IO(t_config*);
