857fc03877c6218e745a64c4cd8563a1ec7459c0
Simón Andrés Cutrera UTN <scutrera@frba.utn.edu.ar>
2024-04-14 18:40:50 -0300

    Update main.c

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -5,14 +5,14 @@
 
 int main(int argc, char* argv[]) {
 
-    char* puerto;
-    t_config* config = iniciar_config("memoria.config", &puerto);
+    t_config* archivo_config = iniciar_config("memoria.config");
+    cargar_config_struct_MEMORIA(archivo_config);
     logger = log_create("memoria.log", "Servidor Memoria", 1, LOG_LEVEL_DEBUG);
     
     decir_hola("Memoria");
 
-    int socket_servidor = iniciar_servidor(puerto);
-    log_info(logger, puerto);
+    int socket_servidor = iniciar_servidor(config.puerto_escucha);
+    log_info(logger, config.puerto_escucha);
     log_info(logger, "Server iniciado bip bop bop bip");
 
     int cliente;  
@@ -33,4 +33,13 @@ int main(int argc, char* argv[]) {
     }
 
     return EXIT_SUCCESS;
+}
+
+void cargar_config_struct_MEMORIA(t_config* archivo_config){
+
+    config.puerto_escucha = config_get_string_value(archivo_config, "PUERTO_ESCUCHA");
+    config.tam_memoria = config_get_string_value(archivo_config, "TAM_MEMORIA");
+    config.tam_pagina = config_get_string_value(archivo_config, "TAM_PAGINA");
+    config.path_instrucciones = config_get_string_value(archivo_config, "PATH_INSTRUCCIONES");
+    config.retardo_respuesta = config_get_string_value(archivo_config, "RETARDO_RESPUESTA");
 }
