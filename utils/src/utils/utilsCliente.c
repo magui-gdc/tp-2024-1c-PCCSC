333a78cd1d0ecad980674eb6a42232b8b0a03622
mcabrera22 <martincho.cabrera22@gmail.com>
2024-04-18 00:31:55 -0300

    ADD: FUNCIONES DE PAQUETES

--- utils/src/utils/utilsCliente.c
+++ utils/src/utils/utilsCliente.c
@@ -1,134 +1,104 @@
-#include "utilsServer.h"
-
-t_log* logger;
+#include "utilsCliente.h"
 
 // CONEXION CLIENTE - SERVIDOR
-//recibe PUERTO, ya que no todos los servidores pueden estar en el mismo puerto
+int crear_conexion(char* ip, char* puerto){
+    struct addrinfo hints;
+    struct addrinfo *server_info;
 
-int iniciar_servidor(char* PUERTO){
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-	int opt = 1;
+    getaddrinfo(ip, puerto, &hints, &server_info);
+    
+    int socket_cliente = socket(
+        server_info->ai_family, 
+        server_info->ai_socktype, 
+        server_info->ai_protocol);
 
-  	int socket_servidor;
-	int err;
-	struct addrinfo hints, *servinfo, *p;
+    // Ahora que tenemos el socket, vamos a conectarlo
+    connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
+    freeaddrinfo(server_info);
 
-	err = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
-
-	socket_servidor = socket(servinfo->ai_family,
-                        	servinfo->ai_socktype,
-                   		    servinfo->ai_protocol);
-
-    if (setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
-    {
-        log_error(logger,"NO SE PUDO LIBERAR EL PUERTO >:(");
-	    exit(EXIT_FAILURE);
-    }
-
-	err = bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
-
-	err = listen(socket_servidor, SOMAXCONN);
-
-	freeaddrinfo(servinfo);
-
-	log_trace(logger, "Listo para escuchar a mi cliente");
-
-	return socket_servidor;
+    return socket_cliente;
 }
 
-int esperar_cliente(int socket_servidor){
-	int socket_cliente = accept(socket_servidor, NULL, NULL);
-	log_info(logger, "Se conecto un cliente!");
 
-	return socket_cliente;
-}
-
-int recibir_operacion(int socket_cliente)
+void enviar_conexion(char* mensaje, int socket_cliente)
 {
-	int cod_op;
-	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
-		return cod_op;
-	else
-	{
-		close(socket_cliente);
-		return -1;
-	}
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+ 
+	paquete->codigo_operacion = CONEXION;
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = strlen(mensaje) + 1;
+	paquete->buffer->stream = malloc(paquete->buffer->size);
+	memcpy(paquete->buffer->stream, mensaje, paquete->buffer->size);
+
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+
+	void* a_enviar = serializar_paquete(paquete, bytes);
+
+	send(socket_cliente, a_enviar, bytes, 0);
+
+	free(a_enviar);
+	eliminar_paquete(paquete);
 }
 
-void* recibir_buffer(int* size, int socket_cliente){
-	void * buffer;
-
-	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
-	buffer = malloc(*size);
-	recv(socket_cliente, buffer, *size, MSG_WAITALL);
-
-	return buffer;
-}
-
-void recibir_conexion(int socket_cliente){
-	int size;
-	char* buffer = recibir_buffer(&size, socket_cliente);
-	log_info(logger, "Se conecto %s", buffer);
-	free(buffer);
-}
-
-void* servidor_escucha(void* conexion){
-	int fd_escucha = *(int*) conexion; // el contenido de la conexion
-	while(1){
-		int *fd_conexion_ptr = malloc(sizeof(int)); // malloc para que por cada cliente aceptado se lo atienda por separado
-		*fd_conexion_ptr = esperar_cliente(fd_escucha);
-		// una vez aceptado, se crea el hilo para manejar la solicitud:
-		pthread_t thread;
-		pthread_create(&thread, NULL, (void*) atender_cliente, fd_conexion_ptr);
-		pthread_detach(thread); // así continúa sin esperar a que finalice el hilo
-	}
-	return NULL;
-}
-
-void* atender_cliente(void* cliente){
-	int cliente_recibido = *(int*) cliente;
-	while(1){
-		int cod_op = recibir_operacion(cliente_recibido);
-		switch (cod_op)
-		{
-		case CONEXION:
-			recibir_conexion(cliente_recibido);
-			break;
-		case -1:
-			log_error(logger, "Cliente desconectado.");
-			close(cliente_recibido); // cierro el socket accept del cliente
-			free(cliente); // libero el malloc reservado para el cliente
-			pthread_exit(NULL); //solo sale del hilo actual => deja de ejecutar la función atender_cliente que lo llamó
-		default:
-			log_warning(logger, "Operacion desconocida.");
-			break;
-		}
-	}
-}
-
-t_list* recibir_paquete(int socket_cliente)
+void eliminar_paquete(t_paquete* paquete)
 {
-	int size;
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+}
+
+void liberar_conexion(int socket_cliente)
+{
+	close(socket_cliente);
+}
+
+// SERIALIZACION
+void* serializar_paquete(t_paquete* paquete, int bytes)
+{
+	void * magic = malloc(bytes);
 	int desplazamiento = 0;
-	void * buffer;
-	t_list* valores = list_create();
-	int tamanio;
 
-	buffer = recibir_buffer(&size, socket_cliente);
-	while(desplazamiento < size)
-	{
-		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
-		desplazamiento+=sizeof(int);
-		char* valor = malloc(tamanio);
-		memcpy(valor, buffer+desplazamiento, tamanio);
-		desplazamiento+=tamanio;
-		list_add(valores, valor);
-	}
-	free(buffer);
-	return valores;
+	memcpy(magic + desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+	desplazamiento+= sizeof(int);
+	memcpy(magic + desplazamiento, &(paquete->buffer->size), sizeof(int));
+	desplazamiento+= sizeof(int);
+	memcpy(magic + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
+	desplazamiento+= paquete->buffer->size;
+
+	return magic;
 }
+
+t_paquete* crear_paquete(void)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = PAQUETE;
+	crear_buffer(paquete);
+	return paquete;
+}
+
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
+{
+	paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
+
+	memcpy(paquete->buffer->stream + paquete->buffer->size, &tamanio, sizeof(int));
+	memcpy(paquete->buffer->stream + paquete->buffer->size + sizeof(int), valor, tamanio);
+
+	paquete->buffer->size += tamanio + sizeof(int);
+}
+
+void enviar_paquete(t_paquete* paquete, int socket_cliente)
+{
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+	void* a_enviar = serializar_paquete(paquete, bytes);
+
+	send(socket_cliente, a_enviar, bytes, 0);
+
+	free(a_enviar);
+}
+
