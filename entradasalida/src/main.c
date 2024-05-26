20bd26e675953ba0c0a34093ffd43a9b995b6c85
magui-gdc <mgonzalezdicarlo@frba.utn.edu.ar>
2024-05-26 19:10:31 +0000

    primerisimos planteos de IO

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -8,12 +8,12 @@ config_struct config;
 int main(int argc, char* argv[]) {
     
     int conexion_kernel, conexion_memoria;
-    t_config* archivo_config = iniciar_config("entradasalida.config");
+    //t_config* archivo_config = iniciar_config("entradasalida.config");
     t_log* logger = log_create("entradasalida.log", "Interfaz I/O", 1, LOG_LEVEL_DEBUG);
 
-    cargar_config_struct_IO(archivo_config);
-
     decir_hola("una Interfaz de Entrada/Salida");
+
+    /////////////////// CONEXIONES //////////////////////
 
     // establecer conexion con KERNEL
     conexion_kernel = crear_conexion(config.ip_kernel, config.puerto_kernel);
@@ -27,6 +27,25 @@ int main(int argc, char* argv[]) {
     paquete(conexion_memoria);
     log_info(logger, "envie paquete a memoria");
 
+    ////////////////////////////////////////////////////////
+
+
+    ///////////////////////// GENERICA /////////////////////////
+
+
+
+    ///////////////////////// STDIN    /////////////////////////
+
+
+
+    //////////////////////// STDOUT    /////////////////////////
+
+
+
+    ///////////////////////// DIALFS   /////////////////////////
+ 
+
+    ///////////////////////////////////////////////////
     log_destroy(logger);
 	config_destroy(archivo_config);
 	liberar_conexion(conexion_kernel);
@@ -35,8 +54,47 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+inicializar_io(char* nombre, t_config* archivo_config){
+    t_io* interfaz = malloc(sizeof(t_io)); //TODO: free(interfaz)
+    interfaz->nombre_id = nombre;
+    selector_carga_config(archivo_config);
+}
 
-void cargar_config_struct_IO(t_config* archivo_config){
+//////////////// CARGAR CONFIG SEGUN DISPOSITIVO IO  ////////////////
+
+///////////////////////// GENÉRICA /////////////////////////
+
+void cargar_config_struct_IO_gen(t_config* archivo_config){
+    config.tipo_interfaz = config_get_string_value(archivo_config, "TIPO_INTERFAZ");
+    config.tiempo_unidad_trabajo = config_get_string_value(archivo_config, "TIEMPO_UNIDAD_TRABAJO");
+    config.ip_kernel = config_get_string_value(archivo_config, "IP_KERNEL");
+    config.puerto_kernel = config_get_string_value(archivo_config, "PUERTO_KERNEL");
+}
+
+///////////////////////// STDIN    /////////////////////////
+
+void cargar_config_struct_IO_in(t_config* archivo_config){
+    config.tipo_interfaz = config_get_string_value(archivo_config, "TIPO_INTERFAZ");
+    config.ip_kernel = config_get_string_value(archivo_config, "IP_KERNEL");
+    config.puerto_kernel = config_get_string_value(archivo_config, "PUERTO_KERNEL");
+    config.ip_memoria = config_get_string_value(archivo_config, "IP_MEMORIA");
+    config.puerto_memoria = config_get_string_value(archivo_config, "PUERTO_MEMORIA");
+}
+
+//////////////////////// STDOUT    /////////////////////////
+
+void cargar_config_struct_IO_out(t_config* archivo_config){
+    config.tipo_interfaz = config_get_string_value(archivo_config, "TIPO_INTERFAZ");
+    config.tiempo_unidad_trabajo = config_get_string_value(archivo_config, "TIEMPO_UNIDAD_TRABAJO");
+    config.ip_kernel = config_get_string_value(archivo_config, "IP_KERNEL");
+    config.puerto_kernel = config_get_string_value(archivo_config, "PUERTO_KERNEL");
+    config.ip_memoria = config_get_string_value(archivo_config, "IP_MEMORIA");
+    config.puerto_memoria = config_get_string_value(archivo_config, "PUERTO_MEMORIA");
+}
+
+///////////////////////// DIALFS   /////////////////////////
+
+void cargar_config_struct_IO_fs(t_config* archivo_config){
     config.tipo_interfaz = config_get_string_value(archivo_config, "TIPO_INTERFAZ");
     config.tiempo_unidad_trabajo = config_get_string_value(archivo_config, "TIEMPO_UNIDAD_TRABAJO");
     config.ip_kernel = config_get_string_value(archivo_config, "IP_KERNEL");
@@ -46,7 +104,11 @@ void cargar_config_struct_IO(t_config* archivo_config){
     config.path_base_dialfs = config_get_string_value(archivo_config, "PATH_BASE_DIALFS");
     config.block_size = config_get_string_value(archivo_config, "BLOCK_SIZE");
     config.block_count = config_get_string_value(archivo_config, "BLOCK_COUNT");
+    config.block_count = config_get_string_value(archivo_config, "RETRASO_COMPACTACION");
+
 }
+
+//////////////////////////////////////////// PRUEBA DE ENVIO DE PAQUETES /////////////////////////////////////////////////////
 
 void paquete(int conexion) {
 	char* leido = malloc(sizeof(char));
@@ -71,4 +133,26 @@ void paquete(int conexion) {
 
 	enviar_paquete(paquete, conexion);
 	eliminar_paquete(paquete);
+}
+
+////////////// SELECTOR DE CARGA DE CONFIG SEGUN INTERFAZ IO ////////////////////////
+
+void selector_carga_config (t_config* archivo_config){
+
+    if(strcmp(config_get_string_value(archivo_config, "TIPO_INTERFAZ"),"GENERICA")){
+        cargar_config_struct_IO_gen(archivo_config);     
+    }
+
+    if(strcmp(config_get_string_value(archivo_config, "TIPO_INTERFAZ"),"STDIN")){
+       cargar_config_struct_IO_in(archivo_config); 
+    }
+
+    if(strcmp(config_get_string_value(archivo_config, "TIPO_INTERFAZ"),"STDOUT")){
+        cargar_config_struct_IO_out(archivo_config);
+    }
+
+    if(strcmp(sconfig_get_string_value(archivo_config, "TIPO_INTERFAZ"),"DIALFS")){
+        cargar_config_struct_IO_fs(archivo_config); 
+    }
+
 }
--- entradasalida/src/main.h
+++ entradasalida/src/main.h
@@ -13,10 +13,34 @@ typedef struct
     char* path_base_dialfs;
     char* block_size;
     char* block_count;
+    char* retraso_compactacion;
 } config_struct;
+
+typedef enum {
+    IO_GEN_SLEEP,
+    IO_STDIN_READ,
+    IO_STDOUT_WRITE,
+    IO_FS_CREATE,
+    IO_FS_DELETE,
+    IO_FS_TRUNCATE
+} instrucciones;
+
+//nos llega el char de la interfaz (mouse, teclado, impresora), y el config que SUPONEMOS que viene del kernel segun la interfaz
+typedef struct
+{
+    char* nombre_id;
+} t_io;
+
 
 extern config_struct config;
 
-void cargar_config_struct_IO(t_config*);
+void cargar_config_struct_IO_gen(t_config*);
+void cargar_config_struct_IO_in(t_config*);
+void cargar_config_struct_IO_out(t_config*);
+void cargar_config_struct_IO_fs(t_config*);
 
 void paquete(int);
+
+void inicializar_io(char*, t_config*);
+
+void selector_carga_config(t_config*);
