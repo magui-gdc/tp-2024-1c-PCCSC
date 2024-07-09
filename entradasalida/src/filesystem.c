613be4d3946793150083f3aa95c1d7dfdf455f9e
magui-gdc <mgonzalezdicarlo@frba.utn.edu.ar>
2024-07-09 15:49:56 +0000

    funciones de creacion archivos

--- entradasalida/src/filesystem.c
+++ entradasalida/src/filesystem.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <string.h>
 #include "filesystem.h"
 #include "main.c"
 
+
+/*          NACIMIENTO            */
+
+void create_bloques_dat(){
+    char * path = strcat(config.path_base_dialfs, "/bloques.dat");
+
+    bloques_dat = fopen(path, "w");
+    if(bloques_dat == NULL){
+        log_error(logger,"Error al inicializar bloques.dat");
+        exit(EXIT_FAILURE);
+    }
+
+}
+
 void init_bitmap_bloques(){
-    size_t tamanio_bitmap = config.block_size*config.block_count; 
+    size_t tamanio_bitmap = config.block_count; 
     
     char *bitmap = malloc(tamanio_bitmap); // tamanio en bytes que se va a reservar
     if (!bitmap) {
@@ -19,4 +34,38 @@ void init_bitmap_bloques(){
     for (size_t i = 0; i < config.block_count; i++) {
         bitarray_clean_bit(bitmap_bloques, i);
     }
+
+    actualizar_bitmap_dat();
+}
+
+void actualizar_bitmap_dat(){
+    char * path = strcat(config.path_base_dialfs, "/bitmap.dat");
+    bitmap_dat = fopen(path, "w");
+    
+    for (size_t i = 0; i < config.block_count; i++) {
+        bool bit = bitarray_test_bit(bitmap_marcos, i);
+        char bit_char = bit + '0';
+        fwrite(bit_char ,sizeof(char),1,bitmap_dat);
+    }
+
+    fclose(bitmap_dat);
+}
+
+void fs_create(){
+    int tamanio_bloque = config.BLOCK_SIZE;
+    
+    create_bloques_dat();
+    init_bitmap_bloques();
+
+}
+
+/*          MUERTE            */
+
+void close_bloques_dat(){
+    fclose(bloques_dat);
+    log_debug(logger, "Archivos bloques.dat cerrado.");
+}
+
+void destroy_bitmap_bloques(){
+    bitarray_destroy(bitmap_bloques);
 }
--- entradasalida/src/filesystem.h
+++ entradasalida/src/filesystem.h
@@ -7,7 +7,16 @@
 #include <semaphore.h>
 
 extern t_bitarray *bitmap_bloques;
+extern FILE* bloques_dat;
+extern FILE* bitmap_dat;
+
+/*          NACIMIENTO            */
 
 void create_bloques_dat();
 void init_bitmap_bloques(); //usar bitarray de las commons
 void init_metadata(); //usar commons como en los config, es un .txt
+
+
+/*          MUERTE       s     */
+void close_bloques_dat();
+void destroy_bitmap_bloques();
